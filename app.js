a66706b875a6e8db99f071655a1bb572860c5cb2
ilhombekk <ilhomsaidov828@mail.com>
2025-12-31 11:06:29 +0500

    add new  button

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const button = document.querySelector(".dark")
-const body = document.body
-let title = document.querySelector(".site__header-title")
+// const button = document.querySelector("#theme-toggle-button")
+// const body = document.querySelector("body")
+// let title = document.querySelector(".site__header-title")
 
-button.addEventListener("click", function() {
-    body.classList.toggle("color-dark");
+// button.addEventListener("click", function() {
+//     body.classList.toggle(".color-dark");
+
+// })
+
+// let toggle = document.getElementById('toggle');
+// let body = document.body;
+// let title = document.querySelector('.site__header-title')
+
+// toggle.addEventListener('change', function() {
+//     body.classList.toggle('color-dark');
+//     title.style.color = 'white'
+// });
+
+let toggle = document.getElementById('toggle');
+let body = document.body;
+let title = document.querySelector('.site__header-title')
+
+toggle.addEventListener('change', function() {
+    body.classList.toggle('color-dark');
     
-    if (body.classList.contains("color-dark")) {
-        title.style.color = "white"
-        button.textContent = "White"
+    if (body.classList.contains('color-dark')) {
+        title.style.color = 'white'; // qora rejimda
     } else {
-        title.style.color = "black"
-        button.textContent = "Dark"
+        title.style.color = 'black'; // oq rejimda (yoki '')
     }
-})
+});
